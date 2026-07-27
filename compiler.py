e6a70fc76c5eb601b365331079e5c47b15b7987c
axolotl <dte.biotech46@gmail.com>
2026-07-27 14:54:11 -0400

    {Read the .esp file}

---
+++ compiler.py
@@ -0,0 +1,20 @@
+"""
+Name: file_read()
+Purpose: Reads the .esp file
+Parameters: none
+Return: espresso_string - the file contents 
+"""
+
+def file_read():
+	
+	espresso_file = open("Main.esp", "r")
+
+	espresso_string = espresso_file.read()
+
+	print(espresso_string)
+
+	espresso_file.close()
+
+	return espresso_string
+
+espresso_string = file_read()
